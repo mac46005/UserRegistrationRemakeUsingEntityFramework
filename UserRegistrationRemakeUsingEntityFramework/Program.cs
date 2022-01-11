507a1d6b39e44851ff41d25808db63c1934c4dc5
Marco Preciado <mac46005@gmail.com>
2022-01-10 22:04:43 -0600

    Recreating user application registration remake

---
+++ UserRegistrationRemakeUsingEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationRemakeUsingEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
